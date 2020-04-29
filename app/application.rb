944354497111f490db5784319f569baa47641b71
BenWix <benwichser@gmail.com>
2020-04-29 08:59:57 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,26 @@
 class Application
 
   def call(env)
+  
+    # resp.write "Hello, World"
+    # resp.finish
+    return [ 200, {'Content-Type' => 'text/html'}, slot_response]
+  end
+
+  def slot_response
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
+    # num_1 = Kernel.rand(20)
+    # num_2 = Kernel.rand(20)
+    # num_3 = Kernel.rand(20)
+    num_1 = 2 
+    num_2 = 2 
+    num_3 = 2
+    
+    if num_1 == num_2 && num_2 == num_3 
+      resp.write "You Win"
+    else 
+      resp.write "You Lose"
+    end
   end
 
 end
